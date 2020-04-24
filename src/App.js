ce8244fc149751b6106f942efb7f06a55e70f349
Saad Salman <saaduhbucs@gmail.com>
2020-04-23 21:18:21 -0400

    Added AddCategory Component, WIP

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import AddItem from "./Components/AddItem";
+import AddCategory from "./Components/AddCategory";
 import ItemList from "./Components/ItemList";
 import "./App.css";
 function App() {
   return (
     <div className="App">
       <h2>Collectable</h2>
+      <AddCategory />
       <AddItem />
       <h3>My Collection: </h3>
       <ItemList />
---
+++ src/Components/Collections.js
@@ -0,0 +1,5 @@
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+import firebase from "../firebase";
